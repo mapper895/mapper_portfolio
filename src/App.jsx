8a74f1cb7b7fff11e9abe102ec9b1f7096f5e491
mapper895 <mapper.895.mp@gmail.com>
2022-09-24 22:43:33 -0500

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Header } from "./components/";
+import { Header, Hero } from "./components/";
 
 const App = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#05c3dd]/80">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#05c3dd]/80">
       <Header />
+
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
     </div>
   );
 };
---
+++ src/components/Hero.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+
+const Hero = () => {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name´s Miguel Perez",
+      "GuyWhoLovesCofee.tsx",
+      "<ButLovesToCodeMore />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <img
+        src="https://cdn.sanity.io/images/jzu4id4u/production/6020beaebd3a0458e9cf4361e0e567c8412adaed-4624x2608.jpg"
+        alt="profile"
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+      />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Telematic Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
+          <span className="mr-3 ">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-5">
+          <Link to="/#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link to="/#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link to="/#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link to="/#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
